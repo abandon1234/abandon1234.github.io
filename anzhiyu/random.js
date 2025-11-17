846b49528cdeeb23d031887bf4c8d5beae9382a2
abandon1234 <99237906+abandon1234@users.noreply.github.com>
2025-11-17 21:14:52 +0000

    Site updated: 2025-11-17 21:14:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/17/hello-world/","2025/11/17/这是一篇新的博文/","2025/11/17/这是另一篇新的博文/"];function toRandomPost(){
+var posts=["ysw/这是另一篇新的博文/","ysw/这是一篇新的博文/","ysw/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
